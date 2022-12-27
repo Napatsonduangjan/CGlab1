c6b7621e98d63c355745b6166f75cd83f3231dc1
Napatsonduangjan <96217177+Napatsonduangjan@users.noreply.github.com>
2022-12-27 18:48:33 +0700

    CGlab1

---
+++ CGlab1/CGlab1/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace CGlab1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.nd = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // nd
+            // 
+            this.nd.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nd.Location = new System.Drawing.Point(675, 385);
+            this.nd.Name = "nd";
+            this.nd.Size = new System.Drawing.Size(89, 38);
+            this.nd.TabIndex = 0;
+            this.nd.Text = "แสดงผล";
+            this.nd.UseVisualStyleBackColor = true;
+            this.nd.Click += new System.EventHandler(this.nd_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.nd);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button nd;
+    }
+}
+
---
+++ CGlab1/CGlab1/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CGlab1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void nd_Click(object sender, EventArgs e)
+        {
+            System.Drawing.Graphics myGraphic;
+            myGraphic = this.CreateGraphics();
+
+            Pen myPen1 = new Pen(System.Drawing.Color.Brown, 5);
+            Pen myPen2 = new Pen(System.Drawing.Color.Black, 5);
+            Pen myPen3 = new Pen(System.Drawing.Color.Pink, 5);
+
+
+
+
+            //myGraphic.DrawLine(myPen1, 50, 50, 200, 50); //จุดเร่ิ่ม จุดสิ้นสุด 
+            //myGraphic.DrawRectangle(myPen1, 15, 35, 170, 140); //จุดมุมบนซ้าย ความกว้าง ความสูงของสี่เหลี่ยม
+            //myGraphic.DrawEllipse(myPen2, 15, 35, 170, 140); //จุดมุมบนซ้าย ความกว้าง ความสูงของกรอบสี่เหลี่ยม
+            //myGraphic.DrawArc(myPen1, 100, 100, 450, 450, 0, 45); //จุดมุมบนซ้าย ความกว้าง ความสูง องศาเริ่ม องศาสิ้นสุด ตามเข็ม
+
+            myGraphic.DrawRectangle(myPen1, 100, 150, 140, 140);
+
+
+            myGraphic.DrawLine(myPen2, 170, 288, 170, 153);
+
+            myGraphic.DrawEllipse(myPen3, 120, 100, 50,50);
+            myGraphic.DrawEllipse(myPen3, 170, 100, 50, 50);
+
+            myGraphic.DrawArc(myPen1, 100, 10, 98,280, 0,45);
+
+
+        }
+    }
+}
